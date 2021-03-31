1d46423fb7816bbfce72098f5c81aabc38bf793c
James McCormack <thejamesmccormack@gmail.com>
2021-03-31 10:57:16 -0700

    add getPaymentDetails and confirmPayment methods

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 require('dotenv').config();
 const privateKey = process.env.PRIVATE_KEY;
 const stripe = require('stripe')(privateKey);
+const { testPaymentId, testEmail } = require('./config');
 
 async function createPayment() {
   const paymentIntent = await stripe.paymentIntents.create({
     amount: 1000,
     currency: 'usd',
     payment_method_types: ['card'],
-    receipt_email: process.env.TEST_EMAIL
+    receipt_email: testEmail
   }).then((res) => {
     console.log("res", res);
   }).catch(err => console.log(err));
+};
+
+async function getPaymentDetails(paymentIntentId) {
+  const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId)
+    .then((res) => {
+      console.log(res);
+    }).catch(err => console.log(err));
+};
+
+async function confirmPayment(paymentIntentId) {
+  const paymentIntent = await stripe.paymentIntents.confirm(
+    paymentIntentId,
+    { payment_method: 'pm_card_visa' }
+  ).then((res) => {
+    console.log(res)
+  }).catch(err => console.log(err));
 }
 
-console.log("createPayment call", createPayment());
+console.log(confirmPayment(testPaymentId));
